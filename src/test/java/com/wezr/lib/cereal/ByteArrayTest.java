f2b3159e9ab6ea52367fc162c79d7a7b35705692
Kai Schutte <schuttek@gmail.com>
2017-06-12 12:40:04 +0200

    test fix loadUnloadInt instead of loadUnloadLong

--- src/test/java/com/wezr/lib/cereal/ByteArrayTest.java
+++ src/test/java/com/wezr/lib/cereal/ByteArrayTest.java
@@ -99,7 +99,7 @@ public class ByteArrayTest {
 	@Test
 	public void intTest() {
 		for (int t = 0; t < randIterations; t++) {
-			assertTrue(loadUnloadLong(rand.nextInt()));
+			assertTrue(loadUnloadInt(rand.nextInt()));
 		}
 		assertTrue(loadUnloadInt(1));
 		assertTrue(loadUnloadInt(0));
