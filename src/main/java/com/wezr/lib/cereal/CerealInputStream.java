89e75674145941c8e82063fe349954ec68c9daf1
Kai Schutte <schuttek@gmail.com>
2017-12-19 11:13:07 +0100

    v0.4.18 added CerealFileSorter

--- src/main/java/com/wezr/lib/cereal/CerealInputStream.java
+++ src/main/java/com/wezr/lib/cereal/CerealInputStream.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 public class CerealInputStream extends InputStream {
 
     private final DataInputStream inputStream;
+    private long position = 0;
 
     public CerealInputStream(InputStream inputStream) {
         this.inputStream = new DataInputStream(inputStream);
@@ -24,12 +25,13 @@ public class CerealInputStream extends InputStream {
 
     public <T> Optional<T> read(Class<T> cerealClass)
             throws InstantiationException, IllegalAccessException, IOException {
+        byte[] byteLengthBuffer = new byte[4];
         try {
-            byte[] byteLengthBuffer = new byte[4];
             inputStream.readFully(byteLengthBuffer);
             int length = ByteArray.bytesToInt(byteLengthBuffer, 0);
             byte[] cerealizedObjectBuffer = new byte[length];
             inputStream.readFully(cerealizedObjectBuffer);
+            position += length + 4;
             ByteArray ba = new ByteArray(cerealizedObjectBuffer);
             T cerealObject = cerealClass.newInstance();
             ((Cerealizable) cerealObject).uncerealizeFrom(ba);
@@ -37,6 +39,15 @@ public class CerealInputStream extends InputStream {
         } catch (EOFException e) {
             return Optional.empty();
         }
+    }
+
+    public long position() {
+        return position;
+    }
+
+    @Override
+    public long skip(final long n) throws IOException {
+        return super.skip(n);
     }
 
     @Override
